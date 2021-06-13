44d385284ef2d3686af1a6e6db6a2fd960d22538
费彬彬 <812129358@qq.com>
2021-06-13 19:32:29 +0800

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/fffbbbbbb/ocr-grpc-server
 go 1.15
 
 require (
+	github.com/satori/go.uuid v1.2.0
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 )
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/exec"
 
 	pb "github.com/fffbbbbbb/ocr-grpc-server/ocr"
+	uuid "github.com/satori/go.uuid"
 	"google.golang.org/grpc"
 )
 
@@ -19,18 +23,54 @@ type server struct {
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.ImageBuffer) (*pb.Captcha, error) {
-	// image := in.GetImage()
-	return &pb.Captcha{Data: "Hello "}, nil
+func (s *server) Getcaptcha(ctx context.Context, in *pb.ImageBuffer) (*pb.Captcha, error) {
+	image := in.GetImage()
+	data, err := getcaptch(&image)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(data)
+	return &pb.Captcha{Data: data}, nil
+}
+
+func getcaptch(image *[]byte) (string, error) {
+	for i := 0; i < 30; i++ {
+		ul := uuid.NewV4()
+		fileName := "./" + ul.String() + ".jepg"
+		f, _ := os.Create(fileName)
+		defer func() {
+			f.Close()
+			os.Remove(fileName)
+		}()
+		_, err := f.Write(*image)
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+
+		cmd := exec.Command("tesseract", fileName, "stdout")
+		// 执行命令，并返回结果
+		output, err := cmd.Output()
+		if err != nil {
+			log.Println(err)
+			return "", err
+		}
+		if len(output) >= 6 {
+			return string(output[:4]), nil
+		}
+	}
+	return "", fmt.Errorf("can not ocr image")
 }
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags) // set flags
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterOcrServerServer(s, &server{})
+	log.Println("service up")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
